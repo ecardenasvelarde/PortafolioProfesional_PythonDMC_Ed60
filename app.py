13488c1b46f3c43bdbbaa0c366b1b38f170d777e
ecardenasvelarde <e.cardenas.velarde@gmail.com>
2026-08-23 09:29:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
+import io
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+st.sidebar.image('Logo.png')
+modulo = st.sidebar.selectbox("Seleccione un Módulo", ["Home","Carga de Dataset"])
+st.sidebar.image('logo_explorar.png')
+if modulo == "Home":
+        st.title("🏦 Análisis Exploratorio de Datos: BankMarketing")
+else:
+    st.title("Carga del dataset")

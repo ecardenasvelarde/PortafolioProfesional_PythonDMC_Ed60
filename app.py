1005cd0b84271b467bc22736b0e98d7f2508adb9
ecardenasvelarde <e.cardenas.velarde@gmail.com>
2026-08-23 09:30:26 -0500

    Update sidebar images in app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import numpy as np
 
 st.sidebar.image('Logo.png')
 modulo = st.sidebar.selectbox("Seleccione un Módulo", ["Home","Carga de Dataset"])
-st.sidebar.image('logo_explorar.png')
+st.sidebar.image('Python_logo.png')
 if modulo == "Home":
         st.title("🏦 Análisis Exploratorio de Datos: BankMarketing")
 else:

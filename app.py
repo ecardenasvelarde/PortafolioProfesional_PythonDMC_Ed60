8d62fe653dec0a9a4f3972f9de62496487597de8
ecardenasvelarde <e.cardenas.velarde@gmail.com>
2026-08-23 09:30:02 -0500

    Remove unused matplotlib and seaborn imports
    Removed unused imports for matplotlib and seaborn.

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ import io
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 
 st.sidebar.image('Logo.png')
 modulo = st.sidebar.selectbox("Seleccione un Módulo", ["Home","Carga de Dataset"])
